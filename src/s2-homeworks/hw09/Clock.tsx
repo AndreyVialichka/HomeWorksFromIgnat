bb3bbe05b21bb2981d985ce91742a8aa72d4eacc
AndreyVialichka <forruexpress12@gmail.com>
2023-04-08 04:35:13 +0300

    hmw-9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -34,8 +34,6 @@ function Clock() {
         setShow(false)
     }
     
-    const hours = date.toLocaleString("ru", { hour:'2-digit' });
-    const minutes = date.toLocaleString("ru", { minute:'2-digit' });
 
     let formatter = new Intl.DateTimeFormat("ru", {
         year: "numeric",
@@ -49,7 +47,7 @@ function Clock() {
     // const month = date.getMonth() + 1;
     // const year = date.getFullYear();
 
-    const stringTime = `${hours}:${minutes}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime = date.toLocaleString("ru", { hour:'2-digit',minute:'2-digit' }) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = `${formatedDate}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
